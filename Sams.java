3fde57256e24f6d16915db364bf68006d34edab0
Abhishek Sharma <abhishekasafpur@gmail.com>
2024-01-26 10:47:27 +0530

    commit by abhishek sharma

--- Sams.java
+++ Sams.java
@@ -1,5 +1,5 @@
 public class Sams {
     public static void main(String[] args){
-    System.out.println("Hello jenkins are you ready to work with me");
+    System.out.println("Hello jenkins are you ready to work with me:Abhishek Sharma");
     }
 }
